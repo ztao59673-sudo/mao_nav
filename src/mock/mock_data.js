18448a29f9b21e685b7ef7e3dbc2cf4ae36135f6
ztao59673-sudo <ztao59673@gmail.com>
2026-04-11 20:24:29 +0800

    chore: 更新导航数据 - 2026/4/11 20:24:29

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -517,6 +517,7 @@ export const mockData = {
       ]
     }
   ],
-  "title": "猫猫导航🐱",
-  "search": "bing"
+  "title": "GPS",
+  "search": "bing",
+  "_fileSha": "83035c696da29fa32a75d9b2cc8a81041f5bf822"
 }
